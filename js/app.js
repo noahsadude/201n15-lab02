975712fefaa79278848bfbb435e08e0d5efa1878
Noah Rhodes <noahmrhodes@gmail.com>
2019-08-13 20:15:29 -0700

    added second question

--- js/app.js
+++ js/app.js
@@ -20,6 +20,18 @@ function fiveQuestions(){
       }
     }
 //second question
-    console.log ('');
+  var guessWoodChuckCapacity = '';
+  while (guessWoodChuckCapacity === ''){
+    guessWoodChuckCapacity = prompt('How much wood could a woodchuck chuck if a woodchuck could chuck wood?');
+    guessWoodChuckCapacity = guessWoodChuckCapacity.toLowerCase();
+    if(guessWoodChuckCapacity === 'too much'){
+      alert('You\'re right! Woodchucks can ingest or \"chuck\" as much as 1/5 of a pound of wood per day, roughly equal to a woodchuck\'s total size. Crazy right?');
+    } else if(guessWoodChuckCapacity === 'not enough'){
+      alert('Guess it depends on your definition. Woodchucks can move approximately 700 pounds of wood per day, per wikipedia.');
+    } else{
+      alert('Invalid entry. Only \"too much\" or "\not enough\" accepted. Try again.');
+      guessWoodChuckCapacity = '';
+    }
   }
+}
 fiveQuestions();
