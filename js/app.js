48fe46a12b3201bfffbb7b3c9826b4374c08bb37
Noah Rhodes <noahmrhodes@gmail.com>
2019-08-13 20:43:57 -0700

    added questions 3-5

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@ function fiveQuestions(){
         guessCurrentHour = '';
       }
     }
+
 //second question
   var guessWoodChuckCapacity = '';
   while (guessWoodChuckCapacity === ''){
@@ -33,5 +34,53 @@ function fiveQuestions(){
       guessWoodChuckCapacity = '';
     }
   }
+
+//third question
+  var hexMath = '';
+  while (hexMath === ''){
+    hexMath = prompt('Is 5 + 5 = A?');
+    hexMath = hexMath.toLowerCase();
+    if(hexMath === 'y'|| hexMath === 'yes'||hexMath === 'true'){
+      alert('Yep! Hexidecimal is super weird right? But it\'s just a different way to look at numbers');
+    } else if(hexMath === 'n'|| hexMath === 'no'|| hexMath === 'false'){
+      alert('Nope. In Hexidecimal land, that is a true statement. Base 16, sucker!');
+    } else{
+      alert('Invalid entry. Only yes/no, true/false, y/n accepted. Try again.');
+      hexMath = '';
+    }
+  }
+
+  //fourth question
+  var binaryMath = '';
+  while (binaryMath === ''){
+    binaryMath = prompt('Lets see if you learned to think outside the box. Is 10 + 11 = 101?');
+    binaryMath = binaryMath.toLowerCase();
+    if(binaryMath === 'y'|| binaryMath === 'yes'||binaryMath === 'true'){
+      alert('Yep! just because it isn\'t in decimal, doesn\'t make it false.');
+    } else if(binaryMath === 'n'|| binaryMath === 'no'|| binaryMath === 'false'){
+      alert('Nope. Binary! Try again!');
+      binaryMath = '';
+    } else{
+      alert('Invalid entry. Only yes/no, true/false, y/n accepted. Try again.');
+      binaryMath = '';
+    }
+  }
+
+  //fifth question
+  var outOfIdeas = '';
+  while (outOfIdeas === ''){
+    outOfIdeas = prompt('Whoo?');
+    outOfIdeas = outOfIdeas.toLowerCase();
+    if(outOfIdeas === 'whoo!'|| outOfIdeas === 'yes'||outOfIdeas === 'true' || outOfIdeas === 'y'){
+      alert('Whoo!');
+    } else if(outOfIdeas === 'boo'|| outOfIdeas === 'no'|| outOfIdeas === 'false' || outOfIdeas === 'false'){
+      alert('Boo!');
+      outOfIdeas = '';
+    } else{
+      alert('Invalid entry. Only whoo/boo, yes/no, true/false, y/n accepted. Try again.');
+      outOfIdeas = '';
+    }
+  }
 }
+
 fiveQuestions();
