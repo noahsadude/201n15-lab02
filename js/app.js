cd1b67b905ec3ff1da7cb343896fe9c4d2d280b0
Noah Rhodes <noahmrhodes@gmail.com>
2019-08-13 19:34:42 -0700

    cleaned up mah q1 while loop. Thanks Nicole!

--- js/app.js
+++ js/app.js
@@ -6,20 +6,20 @@ function fiveQuestions(){
 //first question
   var today = new Date();
   var findCurrentHour = today.getHours();
-  var guessCurrentHour;
-    while (guessCurrentHour != 'y'||guessCurrentHour != 'n'||guessCurrentHour != 'true'||guessCurrentHour != 'false'){
+  var guessCurrentHour = '';
+    while (guessCurrentHour === ''){
       guessCurrentHour = prompt('What hour of the day is it? I read that it is hour '+findCurrentHour);
       guessCurrentHour = guessCurrentHour.toLowerCase();
       if(guessCurrentHour === 'y'|| guessCurrentHour === 'yes'||guessCurrentHour === 'true'){
         alert('Good! Otherwise we would have a problem');
-        break;
       } else if(guessCurrentHour === 'n'|| guessCurrentHour === 'no'|| guessCurrentHour === 'false'){
         alert('Well, your computer clock seems to think otherwise');
-        break;
       } else{
-        alert('Invalid entry. Only yes/no, true/false, y/n accepted. Try again.')
+        alert('Invalid entry. Only yes/no, true/false, y/n accepted. Try again.');
+        guessCurrentHour = '';
       }
     }
-//second question 
-}
+//second question
+    console.log ('');
+  }
 fiveQuestions();
